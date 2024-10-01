16fadad5c148828111a04aef62d9f23547d5b64e
nagesh0137 <nageshsonawane870@gmial.com>
2024-10-01 22:24:47 +0530

    Resolved merge conflicts in package.json and package-lock.json

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'; // Global CSS
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import 'react-toastify/dist/ReactToastify.css';
 
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register(`${process.env.PUBLIC_URL}/service-worker.js`)
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(error => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-    <App /> 
-  </Router>  
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
